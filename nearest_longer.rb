be066140e086e7ea120d52a0273aff0f2a2b4c71
ee92 <eegor650@gmail.com>
2016-12-03 12:42:01 -0800

    first commit

---
+++ nearest_longer.rb
@@ -0,0 +1,30 @@
+#finds nearest larger number in the array
+
+def nearest_larger(arr, idx)
+
+  x = 1
+
+  while x<arr.length
+
+    if arr[idx-x] && arr[idx-x]>arr[idx]
+      return idx-x
+    end
+
+    if arr[idx+x] && arr[idx+x]>arr[idx]
+      return idx+x
+    end
+
+    x = x+1
+
+  end
+
+  return nil
+
+end
+
+# checks either side of index (index ± x ; starting at x=1) 
+# and incrementing x by 1 after checking both sides (- then +)
+# returns the position as soon as a larger number is found,
+# or nil if none is found 
+# 
+# ¯\_(ツ)_/¯
